d8cf2322c91a49edcb8ed70bfde922c472f600af
Bhanuprasad <bhanu@rpwebapps.com>
2025-09-12 11:20:46 +0530

    Comitted

--- app/routers/customer.router.js
+++ app/routers/customer.router.js
@@ -4,7 +4,7 @@ const { authenticateToken, authorizeRole } = require("../middleware/auth.middlew
 module.exports = app => {
     // Protected routes (authentication required)
     app.post('/api/getcustomers', authenticateToken, authorizeRole(['Customer Admin', 'Super Admin']), customersController.getAllCustomers);
-    app.get('/api/customers/:id', authenticateToken, authorizeRole(['Customer Admin', 'Super Admin']), customersController.getCustomerById);
+    app.get('/api/customers/:id', authenticateToken, authorizeRole(['Customer Admin', 'Super Admin','Customer User']), customersController.getCustomerById);
     app.get('/api/customers/email/:email', authenticateToken, authorizeRole(['Customer Admin', 'Super Admin']), customersController.getCustomerByEmail);
     app.get('/api/customers/customer-type/:customerTypeId', authenticateToken, authorizeRole(['Customer Admin', 'Super Admin']), customersController.getCustomersByCustomerType);
     app.post('/api/customers/by-organization', authenticateToken, authorizeRole(['Customer Admin', 'Super Admin']), customersController.getCustomersByOrganization);
